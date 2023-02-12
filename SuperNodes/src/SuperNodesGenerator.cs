96479d3cbdc0f306b2a8ab257f9189efe90f9585
Joanna May <joannamadisonmay@gmail.com>
2023-02-11 22:47:00 -0600

    refactor: add docs

--- SuperNodes/src/SuperNodesGenerator.cs
+++ SuperNodes/src/SuperNodesGenerator.cs
@@ -142,8 +142,47 @@ public record SuperNode(
     => Namespace is not "" ? $"{Namespace}.{Name}" : Name;
 }
 
+/// <summary>
+/// Represents a PowerUp. A PowerUp is a class that can be applied to a
+/// SuperNode. Unlike default interface implementations, PowerUps can add
+/// additional state to a SuperNode, similar to mixins or traits in other
+/// programming languages. Essentially, anything in a PowerUp's source code
+/// is copied over into the implementation of the SuperNode that it is applied
+/// to.
+/// </summary>
+/// <param name="Namespace">Fully qualified namespace containing the node
+/// (without the <c>global::</c> prefix).</param>
+/// <param name="Name">Name of the PowerUp class (not fully qualified). Combine
+/// with <paramref name="Namespace" /> to determine the fully resolved name.
+/// </param>
+/// <param name="FullName">Fully resolved name of the PowerUp class.</param>
+/// <param name="Location">The location of the class declaration syntax node
+/// that corresponds to the PowerUp.</param>
+/// <param name="BaseClass">Fully resolved base class of the PowerUp class,
+/// if any. The base class is used as a constraint when applying the PowerUp
+/// to a SuperNode. If the SuperNode the PowerUp is being applied to isn't a
+/// subtype of the PowerUp's base class, an error is shown to the developer.
+/// </param>
+/// <param name="TypeParameters">An array of the declared type parameters for
+/// the PowerUp class (in order), if any.</param>
+/// <param name="Interfaces">Array of fully qualified interfaces implemented by
+/// the PowerUp class. When applying a PowerUp to a SuperNode, any interfaces
+/// implemented by that PowerUp are also applied to the SuperNode.</param>
+/// <param name="Source">Source text of the PowerUp. This is re-parsed during
+/// generation and adapted into the code needed to apply the PowerUp to a given
+/// SuperNode.</param>
+/// <param name="PropsAndFields">Information about properties and fields
+/// declared in the PowerUp. We track these fields so that static reflection
+/// tables can be generated at build time, allowing scripts and PowerUps to
+/// introspect their properties and fields (and the attributes applied to them)
+/// without having to use reflection.</param>
+/// <param name="Usings">All of the using imports defined for the PowerUp
+/// script.</param>
+/// <param name="HasOnPowerUpMethod">True if the PowerUp has declared a
+/// lifecycle method handler that should be called when the SuperNode it has
+/// been applied to receives a Godot lifecycle notification.</param>
 public record PowerUpDescription(
-  string? Namespace,
+  string Namespace,
   string Name,
   string FullName,
   Location Location,
@@ -155,6 +194,7 @@ public record PowerUpDescription(
   IImmutableSet<string> Usings,
   bool HasOnPowerUpMethod
 ) {
+  /// <summary>True if the PowerUp has generic parameters.</summary>
   public bool IsGeneric => TypeParameters.Length > 0;
 }
 
@@ -323,7 +363,7 @@ public partial class SuperNodesGenerator
       )
       .ToImmutableArray();
 
-    var @namespace = symbol?.ContainingNamespace.ToString();
+    var @namespace = GetContainingNamespace(symbol);
 
     var usings = symbol is not null
       ? GetUsings(symbol)
@@ -379,11 +419,7 @@ public partial class SuperNodesGenerator
     var symbol = model.GetDeclaredSymbol(classDeclaration);
 
     var name = symbol?.Name ?? classDeclaration.Identifier.ValueText;
-    var @namespace = symbol?.ContainingNamespace.IsGlobalNamespace != false
-      ? string.Empty
-      : symbol.ContainingNamespace.ToDisplayString(
-          SymbolDisplayFormat.FullyQualifiedFormat
-        ).Replace("global::", "");
+    var @namespace = GetContainingNamespace(symbol);
 
     var baseClasses = symbol is null
         ? ImmutableArray<string>.Empty
@@ -1005,4 +1041,20 @@ public partial class SuperNodesGenerator
         ).ToArray()
       )
     ).ToImmutableHashSet();
+
+  /// <summary>
+  /// Determines the fully resolved containing namespace of a symbol, if the
+  /// symbol is non-null and has a containing namespace. Otherwise, returns a
+  /// blank string.
+  /// </summary>
+  /// <param name="symbol">A potential symbol whose containing namespace
+  /// should be determined.</param>
+  /// <returns>The fully resolved containing namespace of the sybmol, or the
+  /// empty string.</returns>
+  internal static string GetContainingNamespace(ISymbol? symbol)
+    => symbol?.ContainingNamespace.IsGlobalNamespace != false
+      ? string.Empty
+      : symbol.ContainingNamespace.ToDisplayString(
+          SymbolDisplayFormat.FullyQualifiedFormat
+        ).Replace("global::", "");
 }
