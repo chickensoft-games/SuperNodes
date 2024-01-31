19d6b070708ff0accc352e6343837052b85a2cb4
Zumi Kua <wlzqkwd@Gmail.com>
2024-02-01 01:20:20 +0800

    fix: strip where clauses from the PowerUp code.

---
+++ SuperNodes.TestCases/test/test_cases/GenericWithWhereClause.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Godot;
+using SuperNodes.Types;
+
+public class MyDisposable : IDisposable {
+  public void Dispose() { }
+}
+
+[SuperNode(typeof(GenericWithWhereClause<MyDisposable>))]
+public partial class TestNode : Node {
+  public override partial void _Notification(int what);
+}
+
+[PowerUp]
+public class GenericWithWhereClause<T> where T : IDisposable {
+  public void OnMyPowerUp(int what) { }
+}
+
+//we only need to make sure that this code compiles, no runtime check is required.
